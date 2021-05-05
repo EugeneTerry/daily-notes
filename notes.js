5a4d6e171c98d750a23a7544e6e24161a5543801
EugeneTerry <eugene.terry73@gmail.com>
2021-05-04 22:23:19 -0500

    ok now

---
+++ notes.js
@@ -0,0 +1,17 @@
+const myNotes = [
+  {
+    id: 1,
+    subject: "JavaScript",
+    date: "5/4/2013",
+    felling: "Good",
+    timeSpent: "3 hours"
+  },
+  {
+    id: 2,
+    subject: "CSS",
+    date: "5/5/2013",
+    felling: "OK",
+    timeSpent: "2 hours"
+  }
+]
+console.log(myNotes)
