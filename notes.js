6fa1a9c6c2821e3bf128307d3b24302062fe18a6
EugeneTerry <eugene.terry73@gmail.com>
2021-05-05 19:50:54 -0500

    add note

--- notes.js
+++ notes.js
@@ -3,15 +3,33 @@ const myNotes = [
     id: 1,
     subject: "JavaScript",
     date: "5/4/2013",
-    felling: "Good",
+    feeling: "Good",
     timeSpent: "3 hours"
   },
   {
     id: 2,
     subject: "CSS",
     date: "5/5/2013",
-    felling: "OK",
+    feeling: "OK",
     timeSpent: "2 hours"
   }
 ]
+
+const noteAboutToday = {
+  id: 3,
+    subject: "HTML",
+    date: "5/6/2013",
+    feeling: "Fine",
+    timeSpent: "0.5 hours"
+}
+
+for (notes of myNotes) {
+  console.log(`Note ${notes.id} 
+  I learned ${notes.subject}.
+  I spent ${notes.timeSpent} working on it.
+  I felt ${notes.feeling}.
+  ---------------
+  `)
+}
+
 console.log(myNotes)
